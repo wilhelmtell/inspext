8f5a6a6035215ab01310f2c6ddb18a3b3e098e56
Matan Nassau <matan.nassau@gmail.com>
2010-01-09 23:37:56 -0500

    parse_paragraph: stop on non-CHARACTER_TOKEN
    not only on END_TOKEN.  PARAGRAPH_TOKEN can also mean end of paragraph,
    so does HEADING_TOKEN.

--- parse.c
+++ parse.c
@@ -172,11 +172,7 @@ static node* parse_paragraph(FILE* is, lex_state* lstate, parse_state* pstate)
     while( 1 ) {
         /* FIXME: handle character parsing errors, cleanup */
         tok = sip(is, lstate, pstate);
-        if( tok->type == END_TOKEN ) {
-            putback(tok, pstate);
-            break;
-        }
-        else if( tok->type == CHARACTER_TOKEN && tok->ch == '\n' ) {
+        if( tok->type == CHARACTER_TOKEN && tok->ch == '\n' ) {
             /* FIXME: handle character parsing errors, parsing */
             tmpt = sip(is, lstate, pstate);
             if( tmpt->type == CHARACTER_TOKEN && tmpt->ch == '\n' ) {
@@ -198,10 +194,8 @@ static node* parse_paragraph(FILE* is, lex_state* lstate, parse_state* pstate)
             child_node->type = CHARACTER_NODE;
             free(tok);
         } else if( tok->type != CHARACTER_TOKEN ) {
-            fprintf(stderr, "%s:%d:Unexpected token %s\n",
-                    lstate->filename, lstate->lineno, token_s(tok->type));
-            free(tok);
-            return NULL;
+            putback(tok, pstate);
+            break;
         }
         pos->siblings = child_node;
         pos = pos->siblings;
