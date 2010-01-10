fae6bb564f3edb0bd0ab80fceaa632cc399ccd5a
Matan Nassau <matan.nassau@gmail.com>
2010-01-10 15:40:21 -0500

    token_s(): correct token string representations

--- parse.c
+++ parse.c
@@ -6,13 +6,13 @@
 /* TODO: get rid of this when done */
 static char* token_s(enum token_type t)
 {
-    if( t == HEADING_TOKEN ) return "HEADING";
-    else if( t == CHARACTER_TOKEN ) return "CHARACTER";
-    else if( t == PARAGRAPH_TOKEN ) return "PARAGRAPH";
-    else if( t == INDENT_TOKEN ) return "INDENT";
-    else if( t == END_TOKEN ) return "END";
-    else if( t == UNDEFINED_TOKEN ) return "UNDEFINED";
-    else return "DONNO";
+    if( t == HEADING_TOKEN ) return "HEADING_TOKEN";
+    else if( t == CHARACTER_TOKEN ) return "CHARACTER_TOKEN";
+    else if( t == PARAGRAPH_TOKEN ) return "PARAGRAPH_TOKEN";
+    else if( t == INDENT_TOKEN ) return "INDENT_TOKEN";
+    else if( t == END_TOKEN ) return "END_TOKEN";
+    else if( t == UNDEFINED_TOKEN ) return "UNDEFINED_TOKEN";
+    else return "illegal token";
 }
 
 /* TODO: this functionality should be in scan.c */
