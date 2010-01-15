cbd47a2aab288b6957c9511c485323a2560c9421
Matan Nassau <matan.nassau@gmail.com>
2010-01-14 20:52:39 -0500

    generate latex
    {,sub{,sub}}section and paragraphs.

--- gen_latex.c
+++ gen_latex.c
@@ -1,32 +1,38 @@
 #include "gen_latex.h"
 #include <assert.h>
 
-/* TODO: implement */
-static void gen_latex_heading1(FILE* os, node* syntree)
+static void gen_latex_section(FILE* os, node* syntree, int depth)
 {
+    assert(depth > 0 && depth < 4);
+    fprintf(os, "\n\n");
+    switch( depth ) {
+    case 1:
+        fprintf(os, "\\section{");
+        break;
+    case 2:
+        fprintf(os, "\\subsection{");
+        break;
+    case 3:
+        fprintf(os, "\\subsubsection{");
+        break;
+    }
+    while( syntree != NULL ) {
+        assert(syntree->type == CHARACTER_NODE);
+        fputc(syntree->ch, os);
+        syntree = syntree->siblings;
+    }
+    fprintf(os, "}");
 }
 
-/* TODO: implement */
-static void gen_latex_heading2(FILE* os, node* syntree)
-{
-}
-
-/* TODO: implement */
-static void gen_latex_heading3(FILE* os, node* syntree)
-{
-}
-
-/* TODO: implement */
-static void gen_latex_heading4(FILE* os, node* syntree)
-{
-}
-
-/* TODO: implement */
 static void gen_latex_paragraph(FILE* os, node* syntree)
 {
+    while( syntree != NULL ) {
+        assert(syntree->type == CHARACTER_NODE);
+        fputc(syntree->ch, os);
+        syntree = syntree->siblings;
+    }
 }
 
-/* TODO: implement */
 void gen_latex(FILE* os, node* syntree)
 {
     node *child;
@@ -34,25 +40,13 @@ void gen_latex(FILE* os, node* syntree)
     assert(syntree->type == TEXT_NODE);
     fprintf(os, "\\documentclass{article}[letterpaper,11pt]\n");
     fputc('\n', os);
-    fprintf(os, "\\begin{document}\n");
-    fputc('\n', os);
+    fprintf(os, "\\begin{document}");
     child = syntree->children;
     /* subsequent headings/paragraphs: each is preceded by a couple of
      * delimiting newlines. */
     while( child != NULL ) {
         if( child->type == HEADING_NODE ) {
-            if( child->heading_level == 1 )
-                /* TODO: section? title? */
-                gen_latex_heading1(os, child->children);
-            else if( child->heading_level == 2 )
-                /* TODO: subsection */
-                gen_latex_heading2(os, child->children);
-            else if( child->heading_level == 3 )
-                /* TODO: subsubsection */
-                gen_latex_heading3(os, child->children);
-            else
-                /* TODO: now what? */
-                gen_latex_heading4(os, child->children);
+            gen_latex_section(os, child->children, child->heading_level);
         } else if( child->type == PARAGRAPH_NODE) {
             fputc('\n', os);
             fputc('\n', os);
