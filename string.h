faa945b83c97d2b5238a87c7f6d25d76f3c954d9
Matan Nassau <matan.nassau@gmail.com>
2010-01-16 00:16:26 -0500

    string functions declarations
    Typo in declaration of strnicmp().
    
    Forgot to declare strichr().

--- string.h
+++ string.h
@@ -5,7 +5,8 @@
 
 /* extension for the standard string.h header */
 
+int strnicmp(const char *s1, const char *s2, size_t n);
+char* strichr(const char* str, int c);
 char* stristr(const char* str1, const char* str2);
-int strncmp(const char *s1, const char *s2, size_t n);
 
 #endif // STRING_H_
